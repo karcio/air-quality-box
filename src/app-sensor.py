b5c64418ca9150c20a41ad60f136791c40d812fa
karcio <kczarnecki@hotmail.com>
2021-04-14 23:22:42 +0100

    added name

--- src/app-sensor.py
+++ src/app-sensor.py
@@ -9,18 +9,17 @@ while True:
     pm25 = str(getPm25())
     pm10 = str(getPm10())
 
+    lcd.cursor_pos = (0,4)
+    lcd.write_string('AIR QUALITY')
 
-    lcd.cursor_pos = (0, 0) 
+    lcd.cursor_pos = (2, 0) 
     lcd.write_string('PM2.5: ')
-    lcd.cursor_pos = (0, 7) 
+    lcd.cursor_pos = (2, 7) 
     lcd.write_string(pm25)
 
-    lcd.cursor_pos = (1, 0)
+    lcd.cursor_pos = (3, 0)
     lcd.write_string('PM10: ')
-    lcd.cursor_pos = (1, 7)
+    lcd.cursor_pos = (3, 7)
     lcd.write_string(pm10)
 
-
     time.sleep(5)
-
-
