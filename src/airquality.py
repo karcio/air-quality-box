3039cacf436b95e897c4c4f20af4d20ca1711228
karcio <kczarnecki@hotmail.com>
2021-04-14 23:56:56 +0100

    removed adafruit api

--- src/airquality.py
+++ src/airquality.py
@@ -3,7 +3,6 @@ from Adafruit_IO import Client
 import logging
 
 logging.basicConfig(format=' %(levelname)s - %(asctime)s - %(message)s ', level=logging.INFO)
-aio = Client ('karcio', 'aio_ithi91m18v4RzOGgIGSpM16jSjNg')
  
 ser = serial.Serial('/dev/ttyUSB0')
  
@@ -14,16 +13,12 @@ for index in range(0,10):
 
 def getPm25():
     pmtwofive = int.from_bytes(b''.join(data[2:4]), byteorder='little') / 10
-    #aio.send('air-quality-pm-2-5', pmtwofive)
     logging.info("PM 2.5:\t %s",pmtwofive)
 
     return pmtwofive
 
 def getPm10():
     pmten = int.from_bytes(b''.join(data[4:6]), byteorder='little') / 10
-    #aio.send('air-quality-pm-10', pmten)
     logging.info("PM 10:\t %s",pmten)
 
     return pmten
-
-
