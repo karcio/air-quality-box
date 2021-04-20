10a02bf5197e7181c0ca64bedf0178cc32226bd6
karcio <kczarnecki@hotmail.com>
2021-04-20 23:17:19 +0100

    refactor csv

--- src/app-sensor.py
+++ src/app-sensor.py
@@ -9,29 +9,30 @@ lcd = CharLCD('PCF8574', 0x3f)
 now = '{:%Y%m%d-%H%M%S}'.format(datetime.datetime.now())
 file = 'readings_' + now + '.csv'
 
-with open(file, 'w') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#with open(file, 'a') as csvfile:
+#    filewriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-    while True:
-        lcd.clear()
-        pm25 = str(getPm25())
-        pm10 = str(getPm10())
+while True:
+    lcd.clear()
+    pm25 = str(getPm25())
+    pm10 = str(getPm10())
     
+    lcd.cursor_pos = (0,4)
+    lcd.write_string('AIR QUALITY')
+
+    lcd.cursor_pos = (2, 0) 
+    lcd.write_string('PM2.5: ')
+    lcd.cursor_pos = (2, 7) 
+    lcd.write_string(pm25)
+
+    lcd.cursor_pos = (3, 0)
+    lcd.write_string('PM10: ')
+    lcd.cursor_pos = (3, 7)
+    lcd.write_string(pm10)
+        
+    with open(file, 'a') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
         filewriter.writerow([pm25,pm10])
+        csvfile.close()
 
-        lcd.cursor_pos = (0,4)
-        lcd.write_string('AIR QUALITY')
-
-        lcd.cursor_pos = (2, 0) 
-        lcd.write_string('PM2.5: ')
-        lcd.cursor_pos = (2, 7) 
-        lcd.write_string(pm25)
-
-        lcd.cursor_pos = (3, 0)
-        lcd.write_string('PM10: ')
-        lcd.cursor_pos = (3, 7)
-        lcd.write_string(pm10)
-
-        time.sleep(5)
-
-    filewriter.close()
+    time.sleep(5)
